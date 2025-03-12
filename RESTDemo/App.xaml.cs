c6f7174986d90ac6bef01ece2ef617f3c62f5320
22483403 <22483403@ASN-STU8145>
2025-03-12 14:25:45 +0000

    Add project files.

---
+++ RESTDemo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace RESTDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ RESTDemo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RESTDemo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
