67aaab62a979f0647663ddb6faece8f490757a6d
JoshChadwick <joshuachadwick03@gmail.com>
2025-03-12 14:54:40 +0000

    GET and Response Message

--- RESTDemo/MainViewModel.cs
+++ RESTDemo/MainViewModel.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Text.Json;
 
 namespace RESTDemo
 {
     public class MainViewModel
     {
+        HttpClient client;
+        JsonSerializerOptions _serializerOptions;
+        string baseUrl = "https://67d1962490e0670699babade.mockapi.io";
+        public MainViewModel()
+        {
+            client = new HttpClient();
+            _serializerOptions = new JsonSerializerOptions()
+            {
+                WriteIndented = true,
+            };
+        }
+
+        public ICommand AddUserCommand =>
+            new Command(async () =>
+            {
+                var url = $"{baseUrl}/users";
+                var response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode){
+                    var content = await response.Content.ReadAsStringAsync();
+                }
+            });
     }
 }
